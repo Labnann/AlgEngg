7e3eb79a4802113e35338a911c0d241174ba27ff
Khalid <khalid.masum.92@gmail.com>
2023-01-11 21:45:30 +0600

    test/labtask/cls1: finish task3

--- lab_test/class1/task3_cp.c
+++ lab_test/class1/task3_cp.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 #include "../../include/bst.h"
-#include "../../include/queue.h"
 
-int highest_in_right_till(struct bst_node node, int end){
 
+int highest_in_right_till(struct bst_node *node, int end){
+	if (end < node->value) return node->value;
+	if (end > node->value) return highest_in_right_till(node->right, end);
+	return end;
 
 }
 
 int highest_in_path(struct bst_node* node, int start, int end){
    int rr_case = node->value < start && node->value < end;
-   int ll_case = node->value >= start && node->value >= end;
+   int ll_case = node->value > start && node->value > end;
 
    if (ll_case) return highest_in_path(node->left, start, end);
    if (rr_case) return highest_in_path(node->right, start, end);
@@ -26,27 +28,32 @@ int main() {
 	int number;
 	struct queue *path1, *path2;
 
-	path1 = queue_create();
-	path2 = queue_create();
-
-
-	struct bst_node * bst = NULL;
 	int root = 50;
+	int test_cases;
 
-        bst = bst_create_node(root, NULL);
+	int node_in[1000];
 
-	int node_in[8] = {50, 75, 25, 29, 45, 60, 10, 80};
+	int size = 0, scanned, scanned2;
 
-	int i = 0;
+	do{
+		scanf("%d", &scanned);
+		node_in[size] = scanned;
+		size++;
 
-	while (i<8){
-		struct bst_node *next= bst_create_node(node_in[i], NULL);
-		bst_insert_node(bst, next);
-		i++;
+	}while(scanned != -1);
+
+        struct	bst_node *bst = bst_create_from_array(node_in, size); 	
+
+	scanf("%d", &test_cases);
+	scanned = 10;
+	scanned2 =80;
+
+	while (test_cases--) {
+		 scanf("%d%d", &scanned, &scanned2);
+		 printf("%d\n", highest_in_path(bst, scanned, scanned2));
+		
 	}
-	
 
-	printf("%d", bst_find_node(bst, 29)->value);
 
 
 }
