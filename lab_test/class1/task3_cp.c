25f0b9d1b7da930b0f013fbf897c8f675a61d8fe
Khalid <khalid.masum.92@gmail.com>
2023-01-11 14:45:19 +0600

    lab/cls1 t3 half implnt

--- lab_test/class1/task3_cp.c
+++ lab_test/class1/task3_cp.c
@@ -3,10 +3,26 @@
 #include "../../include/bst.h"
 #include "../../include/queue.h"
 
+int highest_in_right_till(struct bst_node node, int end){
+
+
+}
+
+int highest_in_path(struct bst_node* node, int start, int end){
+   int rr_case = node->value < start && node->value < end;
+   int ll_case = node->value >= start && node->value >= end;
+
+   if (ll_case) return highest_in_path(node->left, start, end);
+   if (rr_case) return highest_in_path(node->right, start, end);
+
+   return highest_in_right_till(node, end);
+
+}
 
 
 
 int main() {
+
 	int number;
 	struct queue *path1, *path2;
 
@@ -19,17 +35,18 @@ int main() {
 
         bst = bst_create_node(root, NULL);
 
-	int node_in = {50, 75, 25, 29, 45, 60, 10, 80};
+	int node_in[8] = {50, 75, 25, 29, 45, 60, 10, 80};
 
 	int i = 0;
 
 	while (i<8){
-		bst_insert_node(node_in[i], NULL);
+		struct bst_node *next= bst_create_node(node_in[i], NULL);
+		bst_insert_node(bst, next);
 		i++;
 	}
 	
 
-	printf("%d", bst_find_node(29)->value);
+	printf("%d", bst_find_node(bst, 29)->value);
 
 
 }
